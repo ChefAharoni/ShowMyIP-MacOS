717c891c683723e1c812f9a9ca6f9bfb0c779c46
Amit Aharoni <ChefAharoni@proton.me>
2024-11-18 19:34:14 -0500

    Initial Commit

---
+++ ShowMyIP/ShowMyIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowMyIPApp.swift
+//  ShowMyIP
+//
+//  Created by Amit Aharoni on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ShowMyIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShowMyIPTests/ShowMyIPTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShowMyIPTests.swift
+//  ShowMyIPTests
+//
+//  Created by Amit Aharoni on 11/18/24.
+//
+
+import Testing
+@testable import ShowMyIP
+
+struct ShowMyIPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
